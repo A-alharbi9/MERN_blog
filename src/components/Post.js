b0f351e0b8bd439fbdaf0876ac452e37b940a237
Abdullah <A21alharbi9@gmail.com>
2021-08-18 19:27:10 +0300

    Add Post component

---
+++ src/components/Post.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import "../styles/post.css";
+const axios = require("axios");
+
+function Post(props) {
+  const [post, setPost] = useState([]);
+
+  // const data = useCallback(() => fetchPostData(), [fetchPostData]);
+
+  useEffect(() => {
+    let isLoaded = false;
+    const getData = () => {
+      return axios
+        .get(`/posts/${props.match.params.id}`)
+        .then((res) => setPost(res.data))
+        .catch((err) => console.log("Error: ", err.message));
+    };
+
+    // fetchPostData();
+    getData();
+    console.log(post);
+    // return () => {
+    //   isLoaded = true;
+    // };
+  }, [post._id]);
+
+  // console.log(props);
+
+  return (
+    <div className="container-fluid ">
+      {post ? (
+        <div
+          className="card d-flex flex-row justify-content-center  align-content-center my-3"
+          id="card"
+          key={post._id}
+        >
+          <div className="card-body body">
+            <h1 className="card-title title">{post.title}</h1>
+            <h4 className="card-text text">{post.body}</h4>
+          </div>
+        </div>
+      ) : (
+        "No post"
+      )}
+    </div>
+  );
+}
+
+export default Post;
