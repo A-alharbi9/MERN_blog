e080b7f2d16c5ecf6908a5af97f43cd6374541d8
Abdullah <A21alharbi9@gmail.com>
2021-08-20 17:41:11 +0300

    Add loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,29 +34,39 @@ function Main() {
 
   return (
     <div className="container">
-      {data.length > 0
-        ? data.map((post) => {
-            return (
-              <div className="card my-3 text-center" key={post._id}>
-                <div className="card-body">
-                  <h4 className="card-title">{post.title}</h4>
-                  <p className="card-subtitle text-muted">{post.snippet}</p>
-                </div>
-                <button className="btn btn-primary" onClick={handleReadMore}>
-                  <Link
-                    className="btn btn-primary"
-                    key={post._id}
-                    to={`/posts/${post._id}`}
-                  >
-                    Read more
-                  </Link>
-                </button>
+      {data.length > 0 ? (
+        data.map((post) => {
+          return (
+            <div className="card my-3 text-center" key={post._id}>
+              <div className="card-body">
+                <h4 className="card-title">{post.title}</h4>
+                <p className="card-subtitle text-muted">{post.snippet}</p>
               </div>
-            );
-          })
-        : "No posts"}
+              <button className="btn btn-primary" onClick={handleReadMore}>
+                <Link
+                  className="btn btn-primary"
+                  key={post._id}
+                  to={`/posts/${post._id}`}
+                >
+                  Read more
+                </Link>
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <div class="text-center">
+          <div
+            class="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      )}
       <button
-        className="btn btn-primary"
+        className="btn btn-primary hidden"
         onClick={(prevState) => prevState + setVisible(3)}
       >
         Show more
--- src/components/Post.js
+++ src/components/Post.js
@@ -44,7 +44,15 @@ function Post(props) {
           </div>
         </div>
       ) : (
-        "No post"
+        <div class="text-center">
+          <div
+            class="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
       )}
     </div>
   );
